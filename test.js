a652a90ca7efed34ba1f595c2a1dc40255dd1499
kana-maruyama <kana.maruyama@jp.sony.com>
2016-02-10 14:28:34 +0900

    Add comments for pid and vid

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-﻿var VID = 0x054C;
-var PID = 0x0B94;
+﻿var VID = 0x054C; // VID for Sony
+var PID = 0x0B94; // PID for test device
 
 var addon = require("./build/Release/usb_dev.node");
 var path = addon.getPath(VID, PID);
